16b967a1beba3c6db54f2a336181f26291a6ca96
TN03 <h.irmler@t-online.de>
2016-11-29 00:57:22 +0100

    remove TinyMCE4

--- source/plugins/tinymce4/config/metaconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$plugin_mcf['tinymce4']['init']="function:tinymce_getInits";
-$plugin_mcf['tinymce4']['utf8_marker']="hidden";
-$plugin_mcf['tinymce4']['CDN_src']="hidden";
-$plugin_mcf['tinymce4']['CDN_version']="hidden";
-//$plugin_mcf['tinymce4']['CDN_version']="function:tinymce_listCdnVersions";
-$plugin_mcf['tinymce4']['CDN_host']="hidden";
-?>
