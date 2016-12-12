5a6a142147a7705031b720619e11f02bafb8c285
TN03 <h.irmler@t-online.de>
2016-12-12 18:35:19 +0100

    turn off autocheck

--- source/plugins/hi_updatecheck/config/config.php
+++ source/plugins/hi_updatecheck/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin_cf['hi_updatecheck']['autocheck']="true";
+$plugin_cf['hi_updatecheck']['autocheck']="false";
 $plugin_cf['hi_updatecheck']['autocheck_timeout']="3";
 $plugin_cf['hi_updatecheck']['autocheck_notify']="All updates";
 $plugin_cf['hi_updatecheck']['ignore']="filebrowser,meta_tags,page_params,tinymce";
